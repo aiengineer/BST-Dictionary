a24cac0ec69018e1e8d9fbdef9579ec9aed2e04e
Soran <soran.gdr.cs@gmail.com>
2018-09-01 21:08:38 +0430

    Implemented class node

--- dictionary.cpp
+++ dictionary.cpp
@@ -8,5 +8,19 @@
 using namespace std;
 
 class node {
+public:
+    string data;
+    string syn;
+    node *parent;
+    node *leftChild;
+    node *rightChild;
+
+    node(){
+        data = "0";
+        syn = "0";
+        parent = NULL;
+        leftChild = NULL;
+        rightChild = NULL;
+    }
 
 };
