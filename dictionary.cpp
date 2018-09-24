01e89556301873a244ddd3975dc292ebf2c12e03
Soran <soran.gdr.cs@gmail.com>
2018-09-24 19:48:00 +0330

    Refactored the codes

--- dictionary.cpp
+++ dictionary.cpp
@@ -27,69 +27,72 @@ public:
 
 class bst {
 public:
-    node* root;
+    node *root;
 
-    bst(){root = NULL;}
+    bst() { root = NULL; }
+
     void insert(string data, string);
-    void display(node*, int);
-    node* search1(string data);
-    node* search2(string data);
+
+    void display(node *, int);
+
+    node *search1(string data);
+
+    node *search2(string data);
 };
 
 void bst::insert(string data, string syn) {
-    node* newNode = new node();
+    node *newNode = new node();
     newNode->data = data;
     newNode->syn = syn;
-    if(root == NULL){
+    if (root == NULL) {
         root = newNode;
-    } else{
-        node* tempNode = new node();
-        node* backTempNode = new node();
+    } else {
+        node *tempNode = new node();
+        node *backTempNode = new node();
         tempNode = root;
-        while (tempNode != NULL){
+        while (tempNode != NULL) {
             backTempNode = tempNode;
-            if(tempNode->data <= newNode->data){
+            if (tempNode->data <= newNode->data) {
                 tempNode = tempNode->rightChild;
-            } else{
+            } else {
                 tempNode = tempNode->leftChild;
             }
         }
         newNode->parent = backTempNode;
-        if(backTempNode->data <= newNode->data){
+        if (backTempNode->data <= newNode->data) {
             backTempNode->rightChild = newNode;
-        } else{
+        } else {
             backTempNode->leftChild = newNode;
         }
     }
 }
 
 void bst::display(node *Node, int level) {
-    if(root == NULL){
-        cout<< "Dictionary is empty.\n";
-    }
-    else{
-        if (Node->rightChild = NULL){
-            display(Node->rightChild, level+1);
+    if (root == NULL) {
+        cout << "Dictionary is empty.\n";
+    } else {
+        if (Node->rightChild = NULL) {
+            display(Node->rightChild, level + 1);
         }
-        if (Node != root){
-            for(int i=0; i < level; i++){
-                cout<<"     ";
+        if (Node != root) {
+            for (int i = 0; i < level; i++) {
+                cout << "     ";
             }
-        } else{
-            cout<<"Root->";
+        } else {
+            cout << "Root->";
         }
-        cout<< Node->data<<": "<< Node->syn<<endl;
-        if (Node->leftChild != NULL){
-            display(Node->leftChild, level+1);
+        cout << Node->data << ": " << Node->syn << endl;
+        if (Node->leftChild != NULL) {
+            display(Node->leftChild, level + 1);
         }
     }
 }
 
-node* bst::search1(string data) {
-    node* tempNode = new node();
+node *bst::search1(string data) {
+    node *tempNode = new node();
     tempNode = root;
-    while (tempNode != NULL){
-        if (tempNode->data == data){
+    while (tempNode != NULL) {
+        if (tempNode->data == data) {
             return tempNode;
         } else {
             if (tempNode->data <= data) {
@@ -102,8 +105,8 @@ node* bst::search1(string data) {
     return NULL;
 }
 
-node* bst::search2(string syn) {
-    node* tempNode = new node();
+node *bst::search2(string syn) {
+    node *tempNode = new node();
     tempNode = root;
     while (tempNode != NULL) {
         if (tempNode->syn == syn) {
@@ -118,3 +121,4 @@ node* bst::search2(string syn) {
     }
     return NULL;
 }
+
